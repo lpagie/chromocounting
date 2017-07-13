6da4ecc2a91ec2986dd1e6d77da478bb4f02848e
Ludo Pagie <l.pagie@nki.nl>
2017-07-13 09:36:23 +0200

    OEfromBAM.R: major update
    * added normalization
    * added correct windowing to compute OE scores
    * various minor changes

--- R/OEfromBAM.R
+++ R/OEfromBAM.R
@@ -1,6 +1,6 @@
 #' @importFrom grDevices bitmap dev.off
 
-#' @importFrom graphics axis barplot box par plot
+#' @importFrom graphics axis barplot box par plot text
 
 #' @importFrom stats lm predict
 
@@ -20,8 +20,8 @@
 #' @param cell.count Integer vector, cell count for all samples.
 #' @param sig Character, string with initials and date used for output file names (eg LP170619).
 #' @param CHR Character vector, chromosome names to be used in the analysis.
-#' @param bin.size Character or integer, "chromlength" (each bin is entire chr
-#' length) or integer specifying the width of the bins used for computing the
+#' @param bin.size integer, 0 (each bin is entire chr
+#' length) or >0 specifying the width in bp of the bins used for computing the
 #' OE scores.
 #' @param fExp Numeric vector, length of twice the number of GATC fragments in
 #' \code{GATC.mpbl.reads} (2 reads per fragment) specifying the (normalized)
@@ -30,11 +30,13 @@
 #' @param OE.norm.factor Numeric vector, normalization factor per window/chromosome to normalize raw OE scores.
 #' @param outdir Character, specifies directory for output files.
 #' @param VERBOSE Logical, print diagnostic msgs [default = FALSE].
+#' @param do.norm Logical, perform normalization [default = FALSE].
 #' 
-#' @return List with 2 elements;
+#' @return List with 3 elements;
 #' \describe{
 #'   \item{OE.norm.factor}{Numeric vector; normalization factor per window/chromosome to normalize raw OE scores}
-#'   \item{OE.norm}{GRanges object; granges specify genomic GATC fragments, mcols specify OE scores for each sample (ie bamfile)}
+#'   \item{OE.norm}{GRanges object; granges specify genomic GATC fragments, mcols specify normalized OE scores for each sample (ie each bamfile)}
+#'   \item{OE.raw}{GRanges object; granges specify genomic GATC fragments, mcols specify raw OE scores for each sample (ie each bamfile)}
 #' }
 #' 
 #' @examples
@@ -48,7 +50,8 @@
 #' @export
 bamToOE <- function(exp.name, bam.dir=getwd(), bam.fname, sample.name, cell.count, 
 		    sig=sprintf("LP%s", format(Sys.time(), "%y%m%d")), CHR, 
-		    bin.size="chromlength", fExp, GATC.mpbl.reads, OE.norm.factor=NULL, outdir, VERBOSE=FALSE) {
+		    bin.size=0, fExp, GATC.mpbl.reads, OE.norm.factor=NULL, outdir, 
+		    VERBOSE=FALSE, do.norm=TRUE) {
 
   # first, check input
   if ( ( length(bam.fname) != length(sample.name) ) || ( length(bam.fname) != length(cell.count) ) )
@@ -73,10 +76,15 @@ bamToOE <- function(exp.name, bam.dir=getwd(), bam.fname, sample.name, cell.coun
   ord <- order(sample.name)
   md <- S4Vectors::DataFrame(bam.fname=bam.fname[ord], sample.name=sample.name[ord], cell.count=cell.count[ord])
 
+  if (VERBOSE) {
+	print("processing bam files with following metadata:")
+	print(md)
+  }
+
   # run bam2Frag
   if (VERBOSE)
     print("starting bam2fragcounts")
-  GATC.fragments.readcounts <- Bam2FragCounts(meta.data=md, bam.dir=".", GATC.mpbl.reads, maxgap=0, shift=0, VERBOSE=VERBOSE)
+  GATC.fragments.readcounts <- Bam2FragCounts(meta.data=md, bam.dir=".", GenomicRanges::granges(GATC.mpbl.reads), maxgap=0, shift=0, VERBOSE=VERBOSE)
   ofname <- file.path(outdir,sprintf("%s_GATC-readcounts_%s.RData", exp.name, sig))
   if(VERBOSE) print(sprintf("Saving GATC.fragments.readcounts to %s", ofname))
   save(file=ofname, x=GATC.fragments.readcounts)
@@ -86,8 +94,11 @@ bamToOE <- function(exp.name, bam.dir=getwd(), bam.fname, sample.name, cell.coun
   # compute OE scores #
   #####################
   # set bin size of calculating OE scores
-  if (bin.size == "chromlength") {
+  if (bin.size == 0) {
     bin.size <- GenomeInfoDb::seqlengths(GATC.fragments.readcounts)
+  } else {
+    if ( any( bin.size>GenomeInfoDb::seqlengths(GATC.fragments.readcounts)[CHR] ) )
+      stop("bin.size larger than a chromosomelength")
   }
 
   # calculate total read count per sample (discard duplicate reads)
@@ -103,13 +114,13 @@ bamToOE <- function(exp.name, bam.dir=getwd(), bam.fname, sample.name, cell.coun
   barplot(-depth, col=1:5, main='-log10(Readdepth) as proportion of mappable reads', ylab='proportion', xlab='sample index')
   dev.off()
   ## optionally; discard samples with relative readdepth < 10^-4
-  ok.idx <- which(depth>-4)
-  ok.idx <- rep(ok.idx-1, each=3)*3+1:3
-  if(! identical(ok.idx, seq_along(S4Vectors::mcols(GATC.fragments.readcounts)))) {
+  if (any (! (depth>-4) ) ) {
+    ok.idx <- which(depth>-4)
+    ok.idx <- rep(ok.idx-1, each=3)*3+1:3
     S4Vectors::mcols(GATC.fragments.readcounts) <- S4Vectors::mcols(GATC.fragments.readcounts)[,ok.idx]
   }
 
-  # coerce readcounts vectors to Rle
+  # coerce readcounts vectors to Rle (to speedup computations)
   for(i in seq.int(ncol(S4Vectors::mcols(GATC.fragments.readcounts)))) {
     S4Vectors::mcols(GATC.fragments.readcounts)[,i] <- S4Vectors::Rle(as.integer(S4Vectors::mcols(GATC.fragments.readcounts)[,i]))
   }
@@ -129,7 +140,8 @@ bamToOE <- function(exp.name, bam.dir=getwd(), bam.fname, sample.name, cell.coun
   # extract data matrix from OE
   OE.mat <- as.matrix(GenomicRanges::as.data.frame(S4Vectors::mcols(OE)))
   # the following expression should not be necessary
-  row.names(OE.mat) <- as.character(GenomeInfoDb::seqnames(OE))
+  rownames(OE.mat) <- paste(as.character(GenomeInfoDb::seqnames(OE)), as.integer(GenomicRanges::start(OE)), sep="_")
+  colnames(OE.mat) <- as.character(names(GenomicRanges::mcols(OE)))
   # generate image with profiles of all OE scores
   ofname <- file.path(outdir, sprintf("%s_OE_raw_profiles_%s.png", exp.name, sig))
   bitmap(file=ofname, res=144, taa=4, width=28, height=21)
@@ -141,53 +153,76 @@ bamToOE <- function(exp.name, bam.dir=getwd(), bam.fname, sample.name, cell.coun
   }
   par(opar)
   dev.off()
-
-  #################
-  # NORMALIZATION #
-  #################
-  # if null model of OE scores is not given, compute here
-  if (is.null(OE.norm.factor)) {
-    # compute avg profile on basis of 'trimmed' subset of OE scores
-    # trimming is based on following thresholds: median(data) +/- 3*mad(data)
-    OE.mat.cleaned <- OE.mat
-    for (r in 1:nrow(OE.mat)) {
-      median <- median(OE.mat[r,])
-      mad <- mad(OE.mat[r,])
-      idx <- which(OE.mat[r,] < median-3*mad | OE.mat[r,] > median+3*mad)
-      OE.mat.cleaned[r,idx] <- NA
-    }
-    OE.null <- rowMeans(OE.mat.cleaned, na.rm=TRUE)
-    rm(OE.mat.cleaned)
-
-    # compute AT per GATC fragment
-    seq.compos <- Biostrings::letterFrequency(x=IRanges::Views(BSgenome.Mmusculus.UCSC.mm10::Mmusculus, 
-							       GenomicRanges::granges(GATC.mpbl.reads)), 
-					      letters=c('AT','CG'))/IRanges::width(GATC.mpbl.reads)
-    # compute mean seq.compos per unit (ie chromosome of window)
-    seq.compos.binned <- tapply(seq.compos[,1], factor(as.character(GenomeInfoDb::seqnames(GATC.mpbl.reads)), levels=CHR), mean)
-
-    OE.norm.factor <- predict(lm(y~x, data.frame(y=OE.null,x=seq.compos.binned)), newdata=data.frame(x=seq.compos.binned))
-  }
-  OE.mat.norm <- OE.mat/OE.norm.factor
-
-  # save normalized OE data
-  ofname <- file.path(outdir, sprintf("%s_OE_norm_%s.RData", exp.name, sig))
-  save(file=ofname, OE.mat.norm)
-  # generate profiles of normalized OE scores, for all samples
-  ofname <- file.path(outdir, sprintf("%s_OE_norm_profiles_%s.png", exp.name, sig))
+  # generate image of sample ID and cell count, as reference
+  ofname <- file.path(outdir, sprintf("%s_OE_profiles_mdata_%s.png", exp.name, sig))
   bitmap(file=ofname, res=144, taa=4, width=28, height=21)
-  opar  <- par(mfrow=rep(ceiling(sqrt(ncol(OE.mat.norm))),2), mar=c(0.5,1,0.5,1))
-  for (i in seq.int(ncol(OE.mat.norm))) {
-    plot(OE.mat.norm, main="", ylim=c(0,2), axes=FALSE, pch=19)
-    axis(2)
+  opar  <- par(mfrow=rep(ceiling(sqrt(ncol(OE.mat))),2), mar=c(0.5,1,0.5,1))
+  for (i in seq.int(ncol(OE.mat))) {
+    plot(NA, main='', xlim=c(-1,1), ylim=c(-1,1), axes=FALSE, pty='n')
+    text(0,0,labels=sprintf("%s\n%d", colnames(OE.mat)[i], md$cell.count[i]), cex=1)
+    # axis(2)
     box()
   }
   par(opar)
   dev.off()
 
-  # coerce OE.mat.norm to GRanges object
-  OE.norm <- OE
-  S4Vectors::mcols(OE.norm) <- S4Vectors::DataFrame(OE.mat.norm)
+  if (!do.norm) {
+    OE.norm.factor <- NULL
+    OE.norm <- NULL
+  } else {
+    #################
+    # NORMALIZATION #
+    #################
+    # if null model of OE scores is not given, compute here
+    if (is.null(OE.norm.factor)) {
+      # compute avg profile on basis of 'trimmed' subset of OE scores
+      # trimming is based on following thresholds: median(data) +/- 3*mad(data)
+      OE.mat.cleaned <- OE.mat
+      for (r in 1:nrow(OE.mat)) {
+        median <- median(OE.mat[r,])
+        mad <- mad(OE.mat[r,])
+        idx <- which(OE.mat[r,] < median-3*mad | OE.mat[r,] > median+3*mad)
+        OE.mat.cleaned[r,idx] <- NA
+      }
+      OE.null <- rowMeans(OE.mat.cleaned, na.rm=TRUE)
+      rm(OE.mat.cleaned)
+  
+      # compute AT per GATC fragment
+      seq.compos <- Biostrings::letterFrequency(
+             x=IRanges::Views(BSgenome.Mmusculus.UCSC.mm10::Mmusculus, 
+        		            GenomicRanges::granges(GATC.mpbl.reads)), 
+  	     letters=c('AT','CG'))/IRanges::width(GATC.mpbl.reads)
+      # compute mean seq.compos per unit (ie chromosome of window)
+      # 1st compute overlap between OE bins and GATC fragments 
+      #   (here seq.compos per OE bin is the mean of seq.compos of all
+      #   GATC.fragments overlapping with the OE.bin, which means some GATC
+      #   fragments may overlap with multiple OE.bins)
+      ovl <- GenomicRanges::findOverlaps(GenomicRanges::granges(GATC.mpbl.reads), GenomicRanges::granges(OE))
+      seq.compos.binned <- aggregate(seq.compos[S4Vectors::queryHits(ovl),1], list(S4Vectors::subjectHits(ovl)), mean)$x
+      OE.norm.factor <- predict(lm(y~x, data.frame(y=OE.null,x=seq.compos.binned)),
+                                newdata=data.frame(x=seq.compos.binned))
+    }
+    OE.mat.norm <- OE.mat/OE.norm.factor
+  
+    # save normalized OE data
+    ofname <- file.path(outdir, sprintf("%s_OE_norm_%s.RData", exp.name, sig))
+    save(file=ofname, OE.mat.norm)
+    # generate profiles of normalized OE scores, for all samples
+    ofname <- file.path(outdir, sprintf("%s_OE_norm_profiles_%s.png", exp.name, sig))
+    bitmap(file=ofname, res=144, taa=4, width=28, height=21)
+    opar  <- par(mfrow=rep(ceiling(sqrt(ncol(OE.mat.norm))),2), mar=c(0.5,1,0.5,1))
+    for (i in seq.int(ncol(OE.mat.norm))) {
+      plot(OE.mat.norm[,i], main="", ylim=c(0,2), axes=FALSE, pch=19)
+      axis(2)
+      box()
+    }
+    par(opar)
+    dev.off()
+  
+    # coerce OE.mat.norm to GRanges object
+    OE.norm <- OE
+    S4Vectors::mcols(OE.norm) <- S4Vectors::DataFrame(OE.mat.norm)
+  }
 
-  return(list(OE.norm.factor=OE.norm.factor, OE.norm=OE.norm))
+  return(list(OE.norm.factor=OE.norm.factor, OE.norm=OE.norm, OE.raw=OE))
 }
